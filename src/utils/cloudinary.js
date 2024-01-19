d7ebdec02505c6ade56e126e7f5ea27d04f498d9
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-19 06:25:30 +0545

    use multer for cloudnary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"       
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  const uploadOnCloudinary = async (localFilePath) => {
+    try {
+      if (!localFilePath) return null;
+      // upload file to cloudinary
+      const response = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto",
+      });
+
+      console.log("file upload sucessfully",response.url);
+      return response;
+  
+      // fs.unlinkSync(localFilePath);
+      // return response;
+    } catch (error) {
+      fs.unlinkSync(localFilePath); // remove file from local storage but than operation is not successful
+      return null;
+    }
+  };
+  
+  export {uploadOnCloudinary}
